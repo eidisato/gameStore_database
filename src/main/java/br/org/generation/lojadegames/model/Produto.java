6d15b02dd3ceead5cc782c53a516f948c9a5585a
eidisato <viniciuseidi@outlook.com>
2021-11-12 18:46:14 -0300

    table user, and security and service

--- src/main/java/br/org/generation/lojadegames/model/Produto.java
+++ src/main/java/br/org/generation/lojadegames/model/Produto.java
@@ -41,6 +41,10 @@ public class Produto {
 	@ManyToOne
 	@JsonIgnoreProperties("produto")
 	private Categoria categoria;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Usuario usuario;
 
 	public long getId() {
 		return id;
@@ -88,6 +92,14 @@ public class Produto {
 
 	public void setCategoria(Categoria categoria) {
 		this.categoria = categoria;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
 	}
 	
 	
